760905c0e7ef2f32c12d6bb04ff13d8b400c08de
YYB1024 <2593819926@qq.com>
2021-04-05 17:50:41 +0800

    测试hello mybatisPlus

--- mybatisplus/src/test/java/com/study/mybatisplus/Hello.java
+++ mybatisplus/src/test/java/com/study/mybatisplus/Hello.java
@@ -10,6 +10,6 @@ package com.study.mybatisplus;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world!");
+        System.out.println("hello mybatisPlus");
     }
 }
