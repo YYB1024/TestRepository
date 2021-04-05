0bcbfb8d2563f5ce3b76cee7925c677882aae891
yangyb <yangyb@wedoctor.com>
2021-04-05 16:38:03 +0800

    测试HelloWord

---
+++ mybatisplus/src/test/java/com/study/mybatisplus/Hello.java
@@ -0,0 +1,15 @@
+package com.study.mybatisplus;
+
+/**
+ * @ProjectName: mybatisplus
+ * @ClassName: Hello
+ * @author：yangyanbin
+ * @Desc：TODO
+ * @date 2021/4/5 16:32
+ * @Version: 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
